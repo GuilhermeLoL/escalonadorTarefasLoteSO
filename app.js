0a38815e6200fb2d20dda335d890698e9710f575
Guilherme Beidaki Peres <guilherme@accellog.com>
2018-06-19 13:43:03 -0300

    Adicionado Menor Primeiro e Maior Prioridade

--- app.js
+++ app.js
@@ -4,25 +4,36 @@ new Vue({
     tempoTotal: 0,
     media: 0,
     tarefas: [
-      {id: 1, nome: 'A', tempo: 10, prioridade: 1, resultado: 0},
-      {id: 2, nome: 'B', tempo: 5, prioridade: 1, resultado: 0},
-      {id: 3, nome: 'C', tempo: 2, prioridade: 1, resultado: 0}
+      {id: 1, nome: 'A', tempo: 10, prioridade: 4, resultado: 0},
+      {id: 2, nome: 'B', tempo: 5, prioridade: 5, resultado: 0},
+      {id: 3, nome: 'C', tempo: 2, prioridade: 2, resultado: 0}
     ]
   },
   methods: {
     atualizarTempoTotal: function (total) {
       this.tempoTotal = total
-      this.media = total / this.tarefas.length
+      this.media = Math.round(total / this.tarefas.length)
     },
-    fifo: function () {
+    fifo: function (reorder) {
       let total = 0
       let tempo = 0
+      if (reorder) {
+        this.tarefas.sort((a, b) => { return a.id - b.id })
+      }
       this.tarefas.forEach(e => {
         tempo += e.tempo
         e.resultado = tempo
         total += tempo
       })
       this.atualizarTempoTotal(total)
+    },
+    menorPrimeiro: function () {
+      this.tarefas.sort((a, b) => { return a.tempo - b.tempo })
+      this.fifo(false)
+    },
+    maiorPrioridade: function () {
+      this.tarefas.sort((a, b) => { return b.prioridade - a.prioridade })
+      this.fifo(false)
     }
   }
 })
