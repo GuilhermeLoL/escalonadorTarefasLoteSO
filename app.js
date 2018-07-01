3c179f35f9f2d4d7c667f1a98ee980fc720e73d8
Guilherme Beidaki Peres <guibperes@gmail.com>
2018-07-01 20:43:06 -0300

    Adicionado Circular

--- app.js
+++ app.js
@@ -12,14 +12,17 @@ new Vue({
   methods: {
     atualizarTempoTotal: function (total) {
       this.tempoTotal = total
-      this.media = Math.round(total / this.tarefas.length)
+      this.media = total / this.tarefas.length
+      this.media = this.media.toFixed(2)
+    },
+    reiniciarLista: function () {
+      this.tarefas.sort((a, b) => { return a.id - b.id })
+      this.tarefas.forEach(e => e.resultado = 0)
     },
     fifo: function (reorder) {
+      if (reorder) this.reiniciarLista()
       let total = 0
       let tempo = 0
-      if (reorder) {
-        this.tarefas.sort((a, b) => { return a.id - b.id })
-      }
       this.tarefas.forEach(e => {
         tempo += e.tempo
         e.resultado = tempo
@@ -28,12 +31,35 @@ new Vue({
       this.atualizarTempoTotal(total)
     },
     menorPrimeiro: function () {
+      this.reiniciarLista()
       this.tarefas.sort((a, b) => { return a.tempo - b.tempo })
       this.fifo(false)
     },
     maiorPrioridade: function () {
+      this.reiniciarLista()
       this.tarefas.sort((a, b) => { return b.prioridade - a.prioridade })
       this.fifo(false)
+    },
+    circular: function () {
+      this.reiniciarLista()
+      let filaExecucao = this.tarefas
+      filaExecucao.forEach(e => e.delete = false)
+      let tq = 0
+      let tempoTotal = 0
+      while (true) {
+        if (filaExecucao.length === 0) break
+        filaExecucao.forEach(e =>{
+          tq++
+          e.resultado++
+          if (e.tempo === e.resultado) {
+            this.tarefas[e.id - 1].resultado = tq
+            e.delete = true
+          }
+        })
+        filaExecucao = filaExecucao.filter(e => {return !e.delete})
+      }
+      this.tarefas.forEach(e => tempoTotal += e.resultado)
+      this.atualizarTempoTotal(tempoTotal)
     },
     rodarTodos: function () {
       this.fifo(true)
@@ -41,6 +67,8 @@ new Vue({
       this.menorPrimeiro()
       console.log(`${this.tempoTotal} - ${this.media}`)
       this.maiorPrioridade()
+      console.log(`${this.tempoTotal} - ${this.media}`)
+      this.circular()
       console.log(`${this.tempoTotal} - ${this.media}`)
     }
   }
